ab86e71f0de8c91e07ca2844d9124631a8bc5174
Thiago Silva <adsl.thiago@gmail.com>
2025-08-07 12:34:02 -0300

    Ajustes de Visualização

--- app/Filament/Resources/TransmissionResource.php
+++ app/Filament/Resources/TransmissionResource.php
@@ -89,7 +89,8 @@ class TransmissionResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\ViewAction::make(),
+                //Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
