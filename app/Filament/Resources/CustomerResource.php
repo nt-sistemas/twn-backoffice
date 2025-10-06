cf1ecc3f13ba3cead4b88f909a373a649b3c9503
Thiago Silva <adsl.thiago@gmail.com>
2025-10-06 12:39:54 -0300

    fix: Gráficos de Notas

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -60,9 +60,37 @@ class CustomerResource extends Resource
                 Forms\Components\TextInput::make('city')
                     ->label('Cidade')
                     ->maxLength(255),
-                Forms\Components\TextInput::make('state')
+                Forms\Components\Select::make('state')
                     ->label('Estado')
-                    ->maxLength(2),
+                    ->options([
+                        'AC' => 'Acre',
+                        'AL' => 'Alagoas',
+                        'AP' => 'Amapá',
+                        'AM' => 'Amazonas',
+                        'BA' => 'Bahia',
+                        'CE' => 'Ceará',
+                        'DF' => 'Distrito Federal',
+                        'ES' => 'Espírito Santo',
+                        'GO' => 'Goiás',
+                        'MA' => 'Maranhão',
+                        'MT' => 'Mato Grosso',
+                        'MS' => 'Mato Grosso do Sul',
+                        'MG' => 'Minas Gerais',
+                        'PA' => 'Pará',
+                        'PB' => 'Paraíba',
+                        'PR' => 'Paraná',
+                        'PE' => 'Pernambuco',
+                        'PI' => 'Piauí',
+                        'RJ' => 'Rio de Janeiro',
+                        'RN' => 'Rio Grande do Norte',
+                        'RS' => 'Rio Grande do Sul',
+                        'RO' => 'Rondônia',
+                        'RR' => 'Roraima',
+                        'SC' => 'Santa Catarina',
+                        'SP' => 'São Paulo',
+                        'SE' => 'Sergipe',
+                        'TO' => 'Tocantins',
+                    ]),
                 Forms\Components\TextInput::make('postal_code')
                     ->label('CEP')
                     ->maxLength(255),
